883eab0dab721e1ff84d2e85754fae5a83044f82
ryu-shen <shenjlong@163.com>
2022-02-20 14:08:28 +0800

    修改脚本

--- encrypt-miner-proxy/Win客户端代理一键安装脚本.bat
+++ encrypt-miner-proxy/Win客户端代理一键安装脚本.bat
@@ -6,8 +6,8 @@ echo **********************************************************************
 echo   请先下载转发主程序https://github.com/ryu0007/EncryptMinerProxy
 echo   本脚本仅支持encrypt-miner-proxy_windows_amd64.exe
 echo   本脚本需配合服务端代理主程序运行，请将脚本与主程序放在同一文件夹下
-echo   登录地址(本地)-http://127.0.0.1:19999/proxy/server/login
-echo   登录地址(外网)-http://{公网IP}:19999/proxy/server/login
+echo   登录地址(本地)-http://127.0.0.1:19999/proxy/client/login
+echo   登录地址(外网)-http://{公网IP}:19999/proxy/client/login
 echo   用户名-ryu
 echo   密码-https://github.com/ryu0007/EncryptMinerProxy
 echo   服务端代理开启的是19999端口，记得开放服务器安全组
@@ -19,8 +19,8 @@ pause
 mode con cols=80 lines=20
 color 1E
 :first
-echo              登录地址(本地)-http://127.0.0.1:19999/proxy/server/login
-echo              登录地址(外网)-http://{公网IP}:19999/proxy/server/login
+echo              登录地址(本地)-http://127.0.0.1:19999/proxy/client/login
+echo              登录地址(外网)-http://{公网IP}:19999/proxy/client/login
 echo              用户名-ryu
 echo              密码-https://github.com/ryu0007/EncryptMinerProxy
 echo             =====================================
@@ -41,7 +41,7 @@ goto first
 echo 正在启动代理
 :设定运行目录为当前文件所在的文件夹
 encrypt-miner-proxy_windows_amd64.exe -client
-echo 执行结束，请登录输入地址，登入WEB：http://{公网IP}:19999/proxy/server/login
+echo 执行结束，请登录输入地址，登入WEB：http://{公网IP}:19999/proxy/client/login
 pause
 goto EXIT
 
