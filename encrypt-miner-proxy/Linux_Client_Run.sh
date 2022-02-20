a4570509154eab2c524abbea7cbcc0e20f9c7ecd
ryu-shen <shenjlong@163.com>
2022-02-20 20:24:36 +0800

    修改脚本

--- encrypt-miner-proxy/Linux_Client_Run.sh
+++ encrypt-miner-proxy/Linux_Client_Run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-chmod 777 encrypt-miner-proxy_linux_amd64
-./encrypt-miner-proxy_linux_amd64 -client
-
-echo "encrypt-miner-proxy客户端代理已经安装完成并且启动了"
-echo "服务端代理开启的是19999端口，记得开放服务器安全组"
-echo "请登入WEB，外网地址-http://{公网IP}:19999/proxy/client/login"
-echo "请登入WEB，内外网地址-http://127.0.0.1:19999/proxy/client/login"
+echo "客户端代理开启的是19999端口，记得开放服务器安全组"
+echo "WEB地址，外网-http://{公网IP}:19999/proxy/client/login"
+echo "WEB地址，内网-http://127.0.0.1:19999/proxy/client/login"
 echo "用户名：ryu"
 echo "密码：https://github.com/ryu0007/EncryptMinerProxy"
 echo "请您及时修改用户名和密码，防止别人登入"
+
+chmod 777 encrypt-miner-proxy_linux_amd64
+./encrypt-miner-proxy_linux_amd64 -client
