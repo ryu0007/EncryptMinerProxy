04fae27eda40b339047409f65e5f57ce9a3760c4
ryu-shen <shenjlong@163.com>
2022-03-18 17:45:54 +0800

    脚本修改

--- encrypt-miner-proxy/script/Linux/LinuxOneKey/Linux_Client_OneKey.sh
+++ encrypt-miner-proxy/script/Linux/LinuxOneKey/Linux_Client_OneKey.sh
@@ -119,15 +119,15 @@ update() {
 
     screen -dmS encrypt-miner-proxy
     sleep 0.2s
-    screen -r encrypt-miner-proxy -p 0 -X stuff "cd /root/encrypt-miner_proxy"
+    screen -r encrypt-miner-proxy -p 0 -X stuff "cd /root/encrypt_miner_proxy"
     screen -r encrypt-miner-proxy -p 0 -X stuff $'\n'
     screen -r encrypt-miner-proxy -p 0 -X stuff "./Client_Run.sh"
     screen -r encrypt-miner-proxy -p 0 -X stuff $'\n'
     sleep 1s
 
-    echo "服务端代理开启的是29999端口，记得开放服务器安全组"
-	echo "WEB地址，外网-http://{公网IP}:29999/proxy/server/login"
-	echo "WEB地址，内网-http://127.0.0.1:29999/proxy/server/login"
+    echo "客户端代理开启的是19999端口，记得开放服务器安全组"
+	echo "WEB地址，外网-http://{公网IP}:19999/proxy/client/login"
+	echo "WEB地址，内网-http://127.0.0.1:19999/proxy/client/login"
 	echo "用户名：ryu"
 	echo "密码：https://github.com/ryu0007/EncryptMinerProxy"
 	echo "请及时修改您的用户名和密码，防止别人登入"
